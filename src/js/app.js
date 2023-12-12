84bd678b5636b4765daa126d8ef22f20ff6a7e8a
Maxym Savonik <msavonik@salerow.software>
2023-12-12 08:59:29 +0200

    added loading

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,26 @@ lazyLoad.lazyLoad();
 $('.header__collapse').on('click', function() {
     $('.header').toggleClass('open');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let vacanciesList = document.querySelector('.vacancies__list');
+    let items = vacanciesList.querySelectorAll('.item');
+    let showMoreButton = document.querySelector('.vacancies__more');
+
+    for (var i = 4; i < items.length; i++) {
+        items[i].style.display = 'none';
+    }
+
+    showMoreButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        // When the "show more" button is clicked, reveal all previously hidden items.
+        for (var i = 3; i < items.length; i++) {
+            items[i].style.display = 'flex';
+        }
+
+        // Then hide the "show more" button.
+        showMoreButton.style.display = 'none';
+    });
+})
+
+
